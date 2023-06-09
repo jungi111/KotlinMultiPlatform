21efc6f6eafbf6b12c323840421dbd51819ee7bf
gihomibin <gihomibin@gmail.com>
2023-06-09 09:06:54 +0900

    update material to material3

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,7 +33,7 @@ kotlin {
                 //put your multiplatform dependencies here
                 implementation(compose.runtime)
                 implementation(compose.foundation)
-                implementation(compose.material)
+                implementation(compose.material3)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
             }
